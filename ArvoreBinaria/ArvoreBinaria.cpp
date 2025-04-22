e220783395d270fc5da019b7d0b48cb6850af36d
GabrielFHE <gabri_el08@hotmail.com>
2025-04-22 11:51:11 -0300

    Estudo de arvore e raiz

--- ArvoreBinaria/ArvoreBinaria.cpp
+++ ArvoreBinaria/ArvoreBinaria.cpp
@@ -152,5 +152,15 @@ int elementosArvore(NO* no)
 
 void exibirElementosArvore(NO* no)
 {
-	
+	if (no == NULL) {
+		return;
+	}
+
+	exibirElementosArvore(no->esq);
+
+	cout << "Valor " << no->valor << endl; 
+
+	exibirElementosArvore(no->dir);
+
+
 }
